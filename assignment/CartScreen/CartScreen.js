66daee5adb667d5634a35259315c0eac5ca3a146
ewurafua <ewurafuaquansah@gmail.com>
2024-07-01 19:31:57 +0930

    cartscreen.js

---
+++ assignment/CartScreen/CartScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import Header from "./Header";
+import Footer from "./Footer";
+import Checkout from "./Checkout";
+
+export default function CartScreen(){
+    return(
+        <View style={styles.container}>
+            <ScrollView styles={styles.scroll}>
+            <Header/>
+            <Checkout/>
+            </ScrollView>
+            <Footer/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+    },
+
+    scroll: {
+        flex: 1,
+    }
+})
